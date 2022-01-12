358a54e25daa466a5242392922d9b7c08829435a
Rizvan Revi <rizvanrp@gmail.com>
2022-01-12 19:40:32 +0800

    Minor UI Design Changes - Visualization

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export class Config {
 
-    XMLnograph = `<root> <mxCell id="0" /> <mxCell id="1" parent="0" /> <mxCell id="5cqd6Tq56_ArgYoLdoSi-1" value="No mxGraph selected." style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1"> <mxGeometry x="40" y="40" width="560" height="40" as="geometry" /> </mxCell> </root>`;
+    XMLnograph = `<root> <mxCell id="0" /> <mxCell id="1" parent="0" /> <mxCell id="5cqd6Tq56_ArgYoLdoSi-1" value="No graph selected." style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1"> <mxGeometry x="40" y="40" width="560" height="40" as="geometry" /> </mxCell> </root>`;
 
     SLAVE_PARAMETER = "Parameter"
 
