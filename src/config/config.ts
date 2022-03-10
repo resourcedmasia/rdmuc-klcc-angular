d74f0fd6a418be6d43f5e50e81a4127483d5d5a3
hafizi hamid <hafizi@rdmasia.com.my>
2022-03-10 11:26:56 +0800

    Add default graph

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,8 @@ export class Config {
 
     XMLnograph = `<root> <mxCell id="0" /> <mxCell id="1" parent="0" /> <mxCell id="5cqd6Tq56_ArgYoLdoSi-1" value="No graph selected." style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1"> <mxGeometry x="40" y="40" width="560" height="40" as="geometry" /> </mxCell> </root>`;
 
+    XMLLoading = `<root> <mxCell id="0" /> <mxCell id="1" parent="0" /> <mxCell id="5cqd6Tq56_ArgYoLdoSi-1" value="Loading.." style="rounded=0;whiteSpace=wrap;html=1;" vertex="1" parent="1"> <mxGeometry x="40" y="40" width="560" height="40" as="geometry" /> </mxCell> </root>`;
+
     DEFAULT_MXGRAPH_STYLESHEET = `../../assets/js/default.xml`;
     
     SLAVE_PARAMETER = "Parameter";
