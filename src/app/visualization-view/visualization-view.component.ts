15c8d0bf4aa8aa021d25a0f4ffd4c230b987b4a3
hafizi hamid <hafizi@rdmasia.com.my>
2022-04-22 11:56:06 +0800

    Fixed alarm

--- src/app/visualization-view/visualization-view.component.ts
+++ src/app/visualization-view/visualization-view.component.ts
@@ -367,7 +367,14 @@ export class VisualizationViewComponent implements OnInit, OnDestroy {
                     setTimeout(() => {
                         this.triggerAlarmOnload($responseArray);
                     }, 3000);
+                } else if ($responseArray.length < result.alarm.length) {
+                  let datas = {
+                    firstOpen: true,
+                    firstClose: true,
+                    alarm: $responseArray
                 }
+                localStorage.setItem('alarm', JSON.stringify(datas));
+              }
             }
         }
      
@@ -416,7 +423,7 @@ export class VisualizationViewComponent implements OnInit, OnDestroy {
 
   triggerAlarmOnload(array: any) {
     let result = JSON.parse(localStorage.getItem('alarm'));
-    if (result.firstOpen === false) {
+    if (result !== null && result.firstOpen === false) {
         let modalService = this.modalService;
         let row = array;
         const options: NgbModalOptions = {
